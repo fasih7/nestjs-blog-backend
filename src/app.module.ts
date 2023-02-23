7603ab6b5dbb3f847749e8f2f22959aa7bc5b7a8
Fasahat Ahmad <fasahat.ahmed@cityscapeone.com>
2023-02-23 11:28:44 +0500

    second

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), PostModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://Blogger:nGSHqY1znr4EuKnA@blogger.jvaggcb.mongodb.net/?retryWrites=true&w=majority',
+    ),
+    PostModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(6000);
+  const PORT = process.env.PORT || 6000;
+  await app.listen(PORT);
 }
 bootstrap();
